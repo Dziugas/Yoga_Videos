37e50ca322eac5532dd1098c1c8e53f0eda8c329
Dziugas <dziugas@graphity.org>
2017-12-30 16:39:10 +0200

    Initial commit

---
+++ new.py
@@ -0,0 +1,33 @@
+import webbrowser
+choice = input ('\n\
+
+Labas, pasirink mankštą! Spausk skaičių ir enter. \n\
+\n\
+1 - \"Gentle Morning Practice\"\n\
+\n\
+2 - \"Yoga To Start Your Day!\"\n\
+\n\
+3 - \"Energizing Morning Sequence\"\n\
+\n\
+4 - \"Yoga For Lower Back Pain\"\n\
+\n\
+5 - \"5-Minute Morning Yoga\"\n\
+\n\
+6 - \"Yoga To Get The Juices Flowing!\"\n\
+\n\
+7 - \"Bedtime Yoga Sequence!\"\n\
+')
+if choice =="1":
+    webbrowser.open('https://www.youtube.com/watch?v=GnHTeHAZQhM')
+elif choice =="2":
+    webbrowser.open('https://youtu.be/Is8tMCpv4F8?t=55')
+elif choice =="3":
+    webbrowser.open('https://www.youtube.com/watch?v=K-Ina_WW4Yc')
+elif choice =="4":
+    webbrowser.open('https://youtu.be/XeXz8fIZDCE?t=54')
+elif choice =="5":
+    webbrowser.open('https://www.youtube.com/watch?v=4C-gxOE0j7s')
+elif choice =="6":
+    webbrowser.open('https://youtu.be/JOilkvadChg?t=37')
+elif choice =="7":
+    webbrowser.open('https://www.youtube.com/watch?v=XOTGz-1vizY')
